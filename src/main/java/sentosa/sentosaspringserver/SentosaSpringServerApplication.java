3865ed10507dfa0af58a44716d03a463adf2391a
pykido <animoto1@naver.com>
2025-01-19 09:56:40 +0900

    feat : 프로젝트 초기 설정

---
+++ src/main/java/sentosa/sentosaspringserver/SentosaSpringServerApplication.java
@@ -0,0 +1,13 @@
+package sentosa.sentosaspringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentosaSpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentosaSpringServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sentosa/sentosaspringserver/SentosaSpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sentosa.sentosaspringserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentosaSpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
