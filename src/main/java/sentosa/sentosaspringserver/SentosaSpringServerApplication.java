435b4d3a3914af9ab75c6c511c532c05a85c3c43
pykido <animoto1@naver.com>
2025-01-20 01:33:08 +0900

    feat : Auditing 기능 활성화

--- src/main/java/sentosa/sentosaspringserver/SentosaSpringServerApplication.java
+++ src/main/java/sentosa/sentosaspringserver/SentosaSpringServerApplication.java
@@ -2,8 +2,10 @@ package sentosa.sentosaspringserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SentosaSpringServerApplication {
 
 	public static void main(String[] args) {
