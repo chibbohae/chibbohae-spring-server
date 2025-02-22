70502a647af6efe302104bf0553e70d8c3cba578
pykido <animoto1@naver.com>
2025-02-22 22:13:39 +0900

    feat : 도커 파일 Entrypoint 수정하기

--- src/main/java/sentosa/sentosaspringserver/global/config/SwaggerConfig.java
+++ src/main/java/sentosa/sentosaspringserver/global/config/SwaggerConfig.java
@@ -12,8 +12,8 @@ public class SwaggerConfig {
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
 			.info(new Info()
-				.title("알리의 손길 Swagger 문서")
-				.description("알리의 손길 API 명세서입니다.")
+				.title("Sentosa Spring Swagger 문서")
+				.description("Sentosa Spring 서버 API 명세서입니다.")
 				.version("1.0"));
 	}
 }
